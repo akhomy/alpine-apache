00a530cf5761322f9afead965bc330d9da15fdc9
Andriy Khomych <andriy.khomych@gmail.com>
2017-05-17 18:01:26 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 set -e
+
+# Copy user defined configs from temp folder to existing.
+if [ "$(ls -A /temp_configs_dir)" ]; then
+  cp -f -R /temp_configs_dir/* /etc/
+fi
+
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
 
