66e733078716508f1a1ac148453c303433d51b68
Andriy Khomych <andriy.khomych@gmail.com>
2017-05-19 01:04:39 +0300

    Updated httpd.conf and added new params to entrypoint for configuring work on the php sites with different subfolders structure.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,11 +11,19 @@ fi
 rm -f /usr/local/apache2/logs/httpd.pid
 
 if [ -n "$PROXY_PASS" ]; then
-     sed -i 's@^    ProxyPass.*@'"    ProxyPass ${PROXY_PASS}"'@' /etc/apache2/httpd.conf
+     sed -i 's@^    ProxyPass.*@'"    ProxyPass ${PROXY_PASS}/\$1"'@' /etc/apache2/httpd.conf
 fi
 
 if [ -n "$APACHE_LISTEN_PORT" ]; then
      sed -i 's@^Listen.*@'"Listen ${APACHE_LISTEN_PORT}"'@' /etc/apache2/httpd.conf
+fi
+
+if [ -n "$DOCUMENT_ROOT" ]; then
+     sed -i 's@^DocumentRoot "/var/www/localhost/htdocs".*@'"DocumentRoot \"${DOCUMENT_ROOT}\""'@' /etc/apache2/httpd.conf
+fi
+
+if [ -n "$DIRECTORY" ]; then
+     sed -i 's@^<Directory "/var/www/localhost/htdocs">.*@'"<Directory \"${DIRECTORY}\">"'@' /etc/apache2/httpd.conf
 fi
 
 httpd -D FOREGROUND
