6cf15d6c24cd9498be859a346738228a0a1aefad
Andriy Khomych <andrii.khomych@lemberg.co.uk>
2017-05-19 10:43:41 +0300

    Updated entry point with set up use config var.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,20 +10,25 @@ fi
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
 
-if [ -n "$PROXY_PASS" ]; then
-     sed -i 's@^    ProxyPass.*@'"    ProxyPass ${PROXY_PASS}/\$1"'@' /etc/apache2/httpd.conf
-fi
+if [ -z "$USE_ONLY_CONFIGS" ]; then
+    
 
-if [ -n "$APACHE_LISTEN_PORT" ]; then
-     sed -i 's@^Listen.*@'"Listen ${APACHE_LISTEN_PORT}"'@' /etc/apache2/httpd.conf
-fi
+    if [ -n "$PROXY_PASS" ]; then
+        sed -i 's@^    ProxyPass.*@'"    ProxyPass ${PROXY_PASS}/\$1"'@' /etc/apache2/httpd.conf
+    fi
 
-if [ -n "$DOCUMENT_ROOT" ]; then
-     sed -i 's@^DocumentRoot "/var/www/localhost/htdocs".*@'"DocumentRoot \"${DOCUMENT_ROOT}\""'@' /etc/apache2/httpd.conf
-fi
+    if [ -n "$APACHE_LISTEN_PORT" ]; then
+        sed -i 's@^Listen.*@'"Listen ${APACHE_LISTEN_PORT}"'@' /etc/apache2/httpd.conf
+    fi
 
-if [ -n "$DIRECTORY" ]; then
-     sed -i 's@^<Directory "/var/www/localhost/htdocs">.*@'"<Directory \"${DIRECTORY}\">"'@' /etc/apache2/httpd.conf
+    if [ -n "$DOCUMENT_ROOT" ]; then
+        sed -i 's@^DocumentRoot "/var/www/localhost/htdocs".*@'"DocumentRoot \"${DOCUMENT_ROOT}\""'@' /etc/apache2/httpd.conf
+    fi
+
+    if [ -n "$DIRECTORY" ]; then
+        sed -i 's@^<Directory "/var/www/localhost/htdocs">.*@'"<Directory \"${DIRECTORY}\">"'@' /etc/apache2/httpd.conf
+    fi
+
 fi
 
 httpd -D FOREGROUND
